38e66f6369d08cf9c220767211345b4f759e3b32
angel <you@example.com>
2023-12-08 02:33:37 -0300

    Adicionar arquivos de projeto.

---
+++ LogicClock/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LogicClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start s = new Start(0, 0, 0);
+            s.Inicio();
+        }
+    }
+}
---
+++ LogicClock/Start.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace LogicClock
+{
+    class Start
+    {
+        private int X { get; set; }
+        private int Y { get; set; }
+        private int Z { get; set; }
+
+        public  Start(int X, int Y, int Z)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+        }
+
+        public void Inicio()
+        {
+            int segundos = 0, minutos = 0, horas = 0;
+
+            while (true)
+            {
+                if (X == 0)
+                {
+                    segundos++;
+                    if (segundos == 60)
+                    {
+                        segundos = 0;
+                        minutos++;
+                        if (minutos == 60)
+                        {
+                            minutos = 0;
+                            horas++;
+                        }
+                    }
+                }
+
+                Console.WriteLine($"Tempo atual: {horas:D2}:{minutos:D2}:{segundos:D2}");
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
